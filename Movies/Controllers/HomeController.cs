6e4802c5cf78bf8e70a9731f6c5ca98892ddbac8
Pongpat.c <C.pongpat@hotmail.com>
2018-07-20 17:05:21 +0700

    Add project files.

---
+++ Movies/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MoviesView mService = new MoviesView();
+            var result = mService.getMaster();
+            return View(result);
+        }
+
+        public ActionResult Season(String id)
+        {
+            MoviesView mService = new MoviesView();
+            var result = mService.getSeaSon(id);
+            return View(result);
+        }
+
+        public ActionResult Episode(String id)
+        {
+
+            MoviesView mService = new MoviesView();
+            var result = mService.getEpisode(id);
+            Totaltime tt = new Totaltime();
+            var Movies_Total_Time = new TimeSpan(result.Sum(r => r.Time.Ticks));
+            tt.total = new TimeSpan(result.Sum(r => r.Time.TimeOfDay.Ticks));
+            return View(tt);
+        }
+        public ActionResult Manage()
+        {
+          
+            return View();
+        }
+        [HttpPost]
+        public JsonResult MasterList()
+        {
+            MoviesView mService = new MoviesView();
+            var result = mService.getMaster();
+            var Obj = new
+            {
+                data = result
+            };
+            var jsonResult = Json(Obj, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            jsonResult.ContentType = "application/json";
+            jsonResult.ContentEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+            return jsonResult;
+          
+        }
+    }
+}
---
+++ Movies/Models/MoviesView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Movies.database;
+
+namespace Movies.Models
+{
+    public class Totaltime
+    {
+        public TimeSpan total { get; set; }
+    }
+    public class MoviesView
+    {
+        public int Movies_ID { get; set; }
+        public String Movies_Name { get; set; }
+        public String Movies_Time { get; set; }
+        public DateTime Time { get; set; }
+        public String Movies_Image { get; set; }
+
+        //public String Movies_Name { get; set; }
+        public List<MoviesView> getMovies()
+        {
+            List<MoviesView> mResult = null;
+            using (var db = new MoviesEntities())
+            {
+                var result = db.SP_GET_MOVIES().ToList();
+                mResult = result.Select(s => new MoviesView
+                {
+                    Movies_Name = s.EPName,
+                    Movies_Time = s.EPTime,
+
+                }).ToList();
+            }
+            return mResult;
+        }
+        public List<MoviesView> getMaster()
+        {
+            List<MoviesView> mResult = null;
+            using (var db = new MoviesEntities())
+            {
+                var result = db.Master.ToList();
+                mResult = result.Select(s => new MoviesView
+                {
+                    Movies_ID = s.id,
+                    Movies_Name = s.Name,
+                    Movies_Image = String.Format("Content/MasterImage/{0}", s.Image),
+                    Movies_Time = s.CreatedOn.HasValue ? s.CreatedOn.Value.ToShortDateString() : "-"
+                }).ToList();
+            }
+            return mResult;
+        }
+        public List<MoviesView> getSeaSon(String id)
+        {
+            List<MoviesView> mResult = new List<MoviesView>();
+            if (id != null)
+            {
+                using (var db = new MoviesEntities())
+                {
+                    var result = db.Season.ToList();
+                    mResult = result.Where(w => w.idMaster == int.Parse(id)).Select(s => new MoviesView
+                    {
+                        Movies_ID = s.id,
+                        Movies_Name = s.Name,
+                        Movies_Image = String.Format("/Content/SeasonImage/{0}", s.Image)
+                    }).ToList();
+                }
+            }
+            return mResult;
+        }
+        public List<MoviesView> getEpisode(String id)
+        {
+            List<MoviesView> mResult = new List<MoviesView>();
+            if (id != null)
+            {
+                using (var db = new MoviesEntities())
+                {
+                    var result = db.Episode.ToList();
+                    mResult = result.Where(w => w.idSeason == int.Parse(id)).Select(s => new MoviesView
+                    {
+                        Movies_ID = s.id,
+                        Movies_Name = s.Name,
+                        Movies_Image = String.Format("/Content/SeasonImage/{0}", s.Image),
+                        Movies_Time = s.Time,
+                        Time = StringToTime(s.Time)
+                    }).ToList();
+                }
+            }
+
+            return mResult;
+        }
+        private DateTime StringToTime(string time)
+        {
+            DateTime dateTime = DateTime.ParseExact(time, "HH:mm:ss", CultureInfo.InvariantCulture);
+            return dateTime;
+        }
+    }
+
+}
